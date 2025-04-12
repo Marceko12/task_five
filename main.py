506c132476b96c044133152cc48c116a45d5ad07
Marceko12 <marcelotorresyalta@gmail.com>
2025-04-11 21:11:23 -0500

    cambios de la rama

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-# Script
-string = "Hola, mundo!"
-print(string)
+#Script conditional numbers
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter another number: "))
+
+
+result = number1 + number2
+print("the sum of the two numbers is: ", result)
